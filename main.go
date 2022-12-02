5877803c956019690944d61f337fa5965b8851f4
Matt Harden <matt.harden@gmail.com>
2022-12-02 06:09:05 +0000

    Make main.go fancier.

--- main.go
+++ main.go
@@ -2,23 +2,35 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/nerdatmath/aoc2022/day1"
 	"github.com/nerdatmath/aoc2022/day2"
 )
 
 func main() {
-	if err := day1.Part1(); err != nil {
-		log.Fatalln(err)
+	if len(os.Args) != 2 {
+		fmt.Fprintln(os.Stderr, "day number required")
+		os.Exit(1)
 	}
-	if err := day1.Part2(); err != nil {
-		log.Fatalln(err)
-	}
-	if err := day2.Part1(); err != nil {
-		log.Fatalln(err)
-	}
-	if err := day2.Part2(); err != nil {
-		log.Fatalln(err)
+	switch os.Args[1] {
+	case "1":
+		if err := day1.Part1(); err != nil {
+			log.Fatalln(err)
+		}
+		if err := day1.Part2(); err != nil {
+			log.Fatalln(err)
+		}
+	case "2":
+		if err := day2.Part1(); err != nil {
+			log.Fatalln(err)
+		}
+		if err := day2.Part2(); err != nil {
+			log.Fatalln(err)
+		}
+	default:
+		log.Fatalf("invalid day number %q", os.Args[1])
 	}
 }
