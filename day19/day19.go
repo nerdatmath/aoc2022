2b9cb7c529c88aa6fb2b76de91274654b201057e
Matt Harden <matt.harden@gmail.com>
2022-12-20 00:38:44 +0000

    Day 19 parsing.

--- day19/day19.go
+++ day19/day19.go
@@ -2,18 +2,42 @@
 package day19
 
 import (
-	"errors"
+	"fmt"
 
 	"github.com/nerdatmath/aoc2022/aoc"
 )
 
-type solution struct{}
+type blueprint struct {
+	id   int
+	or   int
+	cr   int
+	obro int
+	obrc int
+	gro  int
+	grob int
+}
+
+func parseBlueprint(s []byte) (blueprint, error) {
+	bp := blueprint{}
+	_, err := fmt.Sscanf(string(s), "Blueprint %d: Each ore robot costs %d ore. Each clay robot costs %d ore. Each obsidian robot costs %d ore and %d clay. Each geode robot costs %d ore and %d obsidian.",
+		&bp.id, &bp.or, &bp.cr, &bp.obro, &bp.obrc, &bp.gro, &bp.grob)
+	return bp, err
+}
+
+type solution struct {
+	bps []blueprint
+}
 
 func (sol *solution) Parse(s []byte) error {
-	return errors.New("notimplemented")
+	bps, err := aoc.ParseLines(s, parseBlueprint)
+	sol.bps = bps
+	return err
 }
 
 func (sol solution) Part1() {
+	for _, bp := range sol.bps {
+		fmt.Println(bp)
+	}
 }
 
 func (sol solution) Part2() {
